1c76fd2d9dbcaf22adbb53a30f8562dc05a88e4b
srujana-7 <35599507+srujana-7@users.noreply.github.com>
2022-05-02 18:20:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,7 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
 
 app.use(express.json())
 app.use(userRouter)
@@ -30,20 +20,4 @@ app.listen(port, () => {
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
 })
-/*
-const Task = require('./models/task')
-const User = require('./models/user')
-//
 
-const main = async () => {
-    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5c2e4dcb5eac678a23725b5b')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
-*/
